710bbe30f2925013cff2e88a6a848605c1d4dbcd
Ulas Berke <refaniayt@gmail.com>
2024-01-26 13:26:49 +0300

    HesApp Project Files
    Project's Kotlin Files and APK

---
+++ HesApp/app/src/main/java/com/acm431proje/hesapp/Model/Offer.kt
@@ -0,0 +1,4 @@
+package com.acm431proje.hesapp.Model
+
+data class Offer(val company: String, val offerText: String) {
+}
---
+++ HesApp/app/src/main/java/com/acm431proje/hesapp/Model/Service.kt
@@ -0,0 +1,4 @@
+package com.acm431proje.hesapp.Model
+
+data class Service(val name: String, val type: String) {
+}
